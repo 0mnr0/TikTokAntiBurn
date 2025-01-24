55f63ae7c468be9f620d2abc0cbe60acb3cf388b
JustDev <savelijln0@gmail.com>
2025-01-24 09:46:07 +0400

    Fix issuses with keyboard

--- app/src/main/java/made/by/human/tiktokantiburn/MyAccessibilityService.java
+++ app/src/main/java/made/by/human/tiktokantiburn/MyAccessibilityService.java
@@ -1,10 +1,13 @@
 package made.by.human.tiktokantiburn;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityWindowInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import java.util.HashSet;
 import java.util.List;
@@ -13,6 +16,19 @@ import java.util.Set;
 
 public class MyAccessibilityService extends AccessibilityService {
     boolean Launched = false;
+
+    public boolean isKeyboardActive() {
+        List<AccessibilityWindowInfo> windows = getWindows();
+        if (windows == null) return false;
+
+        for (AccessibilityWindowInfo window : windows) {
+            if (window.getType() == AccessibilityWindowInfo.TYPE_INPUT_METHOD) {
+                return true; // Если окно типа INPUT_METHOD активно, значит клавиатура открыта
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -29,7 +45,8 @@ public class MyAccessibilityService extends AccessibilityService {
 
 
             Intent serviceIntent = new Intent(this, FloatingWindowService.class);
-            if (!activePackages.contains("com.zhiliaoapp.musically")){
+            Toast.makeText(this, "isKeyboardActive: "+isKeyboardActive(), Toast.LENGTH_SHORT).show();
+            if (!activePackages.contains("com.zhiliaoapp.musically") || isKeyboardActive()){
                 serviceIntent.setAction("ACTION_CLOSE_WINDOW");
                 startService(serviceIntent);
             } else {
