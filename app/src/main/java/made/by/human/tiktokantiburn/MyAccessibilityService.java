5e33bd08fd7dea7ae0e551663af8a9f341921791
JustDev <savelijln0@gmail.com>
2025-03-07 11:04:36 +0400

    Some stability improvements

--- app/src/main/java/made/by/human/tiktokantiburn/MyAccessibilityService.java
+++ app/src/main/java/made/by/human/tiktokantiburn/MyAccessibilityService.java
@@ -2,21 +2,13 @@ package made.by.human.tiktokantiburn;
 
 import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
-import android.graphics.Rect;
 import android.util.Log;
-import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityWindowInfo;
-
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 
 public class MyAccessibilityService extends AccessibilityService {
@@ -28,24 +20,30 @@ public class MyAccessibilityService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            List<AccessibilityWindowInfo> windows = getWindows();
+            Intent serviceIntent = new Intent(this, FloatingWindowService.class);
+            try {
+                List<AccessibilityWindowInfo> windows = getWindows();
 
-            Set<String> activePackages = new HashSet<>();
-            for (AccessibilityWindowInfo window : windows) {
-                if (window.getRoot() != null) {
-                    if (window.getRoot().getPackageName() != null) {
-                        String packageName = window.getRoot().getPackageName().toString();
-                        activePackages.add(packageName);
+                Set<String> activePackages = new HashSet<>();
+                for (AccessibilityWindowInfo window : windows) {
+                    if (window.getRoot() != null) {
+                        if (window.getRoot().getPackageName() != null) {
+                            String packageName = window.getRoot().getPackageName().toString();
+                            activePackages.add(packageName);
+                        }
                     }
                 }
-            }
 
-            Intent serviceIntent = new Intent(this, FloatingWindowService.class);
-            Log.w("TikTok is NOT opened: ", String.valueOf(!activePackages.contains("com.zhiliaoapp.musically")));
-            if (!activePackages.contains("com.zhiliaoapp.musically")){
+                Log.w("TikTok is NOT opened: ", String.valueOf(!activePackages.contains("com.zhiliaoapp.musically")));
+                if (!activePackages.contains("com.zhiliaoapp.musically")) {
+                    serviceIntent.setAction("ACTION_CLOSE_WINDOW");
+                    startService(serviceIntent);
+                } else {
+                    startService(serviceIntent);
+                }
+            } catch (Exception e) {
+                Log.w("Exception caught:", e);
                 serviceIntent.setAction("ACTION_CLOSE_WINDOW");
-                startService(serviceIntent);
-            } else {
                 startService(serviceIntent);
             }
 
