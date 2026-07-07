f1811abf4a58384f9fca947ac54a211dd35b3d2b
dsvl0 <savelijln0@gmail.com>
2026-07-07 15:22:34 +0400

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "1.5.3"
+        versionName = "1.5.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
